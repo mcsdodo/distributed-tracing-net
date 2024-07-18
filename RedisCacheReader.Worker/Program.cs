953e34f75c51a5775d856497adb4bb8bf39a7fdb
Balazs Nagy <bnagy@sygic.com>
2024-07-18 10:33:25 +0200

    feat: redis cache reader worker

---
+++ RedisCacheReader.Worker/Program.cs
@@ -0,0 +1,51 @@
+using Common.Redis;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using RedisCacheReader.Worker;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using StackExchange.Redis;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+
+builder.Services.Configure<Connections>(builder.Configuration.GetSection("Connections"));
+
+builder.Services.AddLogging(configure =>
+{
+    configure.AddConsole();
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("redisstreamreader.worker"))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .SetSampler<AlwaysOnSampler>()
+            .AddSource("Redis.Cache.Consumer")
+            .AddHttpClientInstrumentation()
+            .AddRedisInstrumentation()
+            .AddOtlpExporter();
+        
+        tracing.ConfigureRedisInstrumentation((services, configure) =>
+        {
+            var nonKeyedLazyMultiplexer =
+                services.GetRequiredService<Lazy<IConnectionMultiplexer>>();
+            configure.AddConnection("Multiplexer", nonKeyedLazyMultiplexer.Value);
+        });
+    });
+
+builder.Services.TryAddSingleton<Lazy<IConnectionMultiplexer>>(provider =>
+{
+    var connectionString = provider.GetRequiredService<IOptions<Connections>>().Value.Redis.ConnectionString;
+    return new Lazy<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(connectionString));
+});
+
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.TryAddSingleton<IRedisCacheService, RedisCacheService>();
+
+
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ RedisCacheReader.Worker/Worker.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using System.Text.Json;
+using Common.Redis;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+
+namespace RedisCacheReader.Worker;
+
+public class Worker : BackgroundService
+{
+    private readonly IRedisCacheService _redisCacheService;
+    private readonly ILogger<Worker> _logger;
+    private readonly ActivitySource _activitySource = new("Redis.Cache.Consumer");
+
+    public Worker(IRedisCacheService redisCacheService, ILogger<Worker> logger)
+    {
+        _redisCacheService = redisCacheService;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker 1 running at: {time}", DateTimeOffset.Now);
+            }
+
+            var value = _redisCacheService.Get("redis-key");
+
+            if (value == null)
+            {
+                _logger.LogInformation("Cache not found");
+                continue;
+            }
+
+            var message = JsonSerializer.Deserialize<TracedMessage>(value);
+
+            if (message == null)
+            {
+                _logger.LogError("Message could not be deserialized");
+                continue;
+            }
+
+            // Extract context from message and start activity using it
+            var parentContext = Propagators.DefaultTextMapPropagator.Extract(default, message, (message, key) => [message.PropagationContext]);
+            Baggage.Current = parentContext.Baggage;
+                
+            // Start the activity earlier and set the context when we have access to it? ¯\_(ツ)_/¯
+            using var activity = _activitySource.StartActivity("redis-consume", ActivityKind.Consumer, parentContext.ActivityContext);
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
